b20f843561577a63ebb25c759d3fd844fde1ceb8
FROZEN-nyako <136965989+FROZEN-nyako@users.noreply.github.com>
2024-10-15 21:12:42 +0800

    Delete parity.py

--- parity.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def P(v, n):
-    u = np.empty(n ** 3)
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                u[n ** 2 * i + n * j + k] = v[n ** 2 * (n - 1 - i) + n * (n - 1 - j) + (n - 1 - k)].real
-    return u
